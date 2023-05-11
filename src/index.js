efac45ec4d25a0db89e21a0ba18c6c152f14a514
virtualOicenter <virtual@oicenter.co.il>
2023-05-11 13:38:46 +0300

    hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import './index.css';
 
@@ -13,9 +13,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 reportWebVitals();
