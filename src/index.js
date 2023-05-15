d8645258f6a60e731fe479d5d7ad838bf5547a95
virtualOicenter <virtual@oicenter.co.il>
2023-05-15 13:58:43 +0300

    added basename as chatGPT suggested

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import './index.css';
 
@@ -13,9 +13,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/dnd-3dmodel-quiz">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 reportWebVitals();
