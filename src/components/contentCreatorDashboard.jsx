c46152c224b33b4091471037991cc4661d19c11d
Yoni Vas <59569912+BarakSaban@users.noreply.github.com>
2023-06-30 00:57:30 +0300

    added keys in dashboard

--- src/components/contentCreatorDashboard.jsx
+++ src/components/contentCreatorDashboard.jsx
@@ -12,9 +12,9 @@ import { FetchExercises } from './fetchWixData';
 
 const emptyExercise =
 {
-    "model": {'_id':'new'},
+    "model": { '_id': 'new' },
     "info": "",
-    "hotspotsFile": {'_id':'new'},
+    "hotspotsFile": { '_id': 'new' },
     "title": "",
     "tags": null,
     "isPublished": false
@@ -24,31 +24,31 @@ const emptyExercise =
 function ContentCreatorDashBoard() {
     const [newExerciseDialogVisible, setNewExerciseDialogVisible] = useState(false)
     const [exercisesArr, setExercisesArr] = useState(null)
-    const [isLoaded,setIslLoaded] = useState(false)
+    const [isLoaded, setIslLoaded] = useState(false)
     useEffect(() => {
         const fetchExercises = async () => {
-          try {
-            const fetchedExercisesArr = await FetchExercises();
-            setExercisesArr(fetchedExercisesArr);
-            setIslLoaded(true)
-          } catch (error) {
-            console.error('Error fetching data', error);
-          }
+            try {
+                const fetchedExercisesArr = await FetchExercises();
+                setExercisesArr(fetchedExercisesArr);
+                setIslLoaded(true)
+            } catch (error) {
+                console.error('Error fetching data', error);
+            }
         };
-      
+
         fetchExercises();
-      }, [newExerciseDialogVisible]);
+    }, [newExerciseDialogVisible]);
 
 
     return (
-        <div className='contentCreatorDashboard flex flex-column align-items-center w-15' 
-        style={{direction:'rtl'}}>
+        <div className='contentCreatorDashboard flex flex-column align-items-center w-15'
+            style={{ direction: 'rtl' }}>
             <span className='flex flex-column vertical-align-baseline w-9'>
                 <h3>שלום יוני וסילבסקי</h3>
                 <div className='flex flex-row justify-content-center align-items-center w-auto gap-3 mx-5'>
                     <label className='w-3'>חפש פעילות</label>
                     <AutoComplete className='w-full h-3rem' />
-                    <Button className='w-3 h-2rem' label='פעילות חדשה' onClick={() => {setNewExerciseDialogVisible(true);setIslLoaded(false)}} />
+                    <Button className='w-3 h-2rem' label='פעילות חדשה' onClick={() => { setNewExerciseDialogVisible(true); setIslLoaded(false) }} />
                     <Dialog header="הגדרת פעילות" visible={newExerciseDialogVisible} onHide={() => setNewExerciseDialogVisible(false)}
                         headerStyle={{ direction: 'rtl' }} className='w-9' >
                         {ExcerciseDefinition(emptyExercise)}
@@ -56,17 +56,17 @@ function ContentCreatorDashBoard() {
                 </div>
             </span>
             <Card className='min-w-7 min-h-3 p-3 m-3 '>
-                <Fieldset legend='פעילויות מוכנות לפרסום'  toggleable>
+                <Fieldset legend='פעילויות מוכנות לפרסום' toggleable key="published exercises">
                     <div className='unitCardsGrid overflow-y-scroll min-h-1 p-1'>
                         {exercisesArr && exercisesArr.filter(f => f.isPublished).map((exercise, index) => {
-                            return <ExerciseCard key={index} exerciseData={exercise}  setExercisesArr={setExercisesArr} />
+                            return <ExerciseCard key={`published${index}`} exerciseData={exercise} setExercisesArr={setExercisesArr} />
                         })}
                     </div>
                 </Fieldset>
-                <Fieldset legend='פעילויות בהכנה'  toggleable>
+                <Fieldset legend='פעילויות בהכנה' toggleable keyy="unpublished exercises">
                     <div className='unitCardsGrid overflow-y-scroll min-h-1 max-h-25rem p-1'>
                         {exercisesArr && exercisesArr.filter(f => !f.isPublished).map((exercise, index) => {
-                            return <ExerciseCard key={index} exerciseData={exercise} setExercisesArr={setExercisesArr} />
+                            return <ExerciseCard key={`unpublished${index}`} exerciseData={exercise} setExercisesArr={setExercisesArr} />
                         })}
                     </div>
                 </Fieldset>
